79d19deddd2d64a8a784ffd0b76ec876d3fe11d0
yordan-gergov01 <yordansoftskills@gmail.com>
2025-04-03 19:32:32 +0300

    implementing encryption and decryption functionality

--- decryption.js
+++ decryption.js
@@ -1,12 +1,12 @@
 const { Transform } = require("node:stream");
 const fs = require("node:fs/promises");
 
-class Encrypt extends Transform {
+class Decrypt extends Transform {
   _transform(chunk, encoding, callback) {
-    // <34 + 1, ff, a4 + 1, 11 + 1, 22 + 1....>
+    // <35 - 1, ff, a5 - 1, 12 - 1, 23 - 1....>
     for (let i = 0; i < chunk.length; ++i) {
       if (chunk[i] !== 255) {
-        chunk[i] = chunk[i] + 1;
+        chunk[i] = chunk[i] - 1;
       }
     }
     callback(null, chunk);
@@ -14,13 +14,13 @@ class Encrypt extends Transform {
 }
 
 (async () => {
-  const readFileHandle = await fs.open("read.txt", "r");
-  const writeFileHandle = await fs.open("write.txt", "w");
+  const readFileHandle = await fs.open("write.txt", "r");
+  const writeFileHandle = await fs.open("decrypted.txt", "w");
 
   const readStream = readFileHandle.createReadStream();
   const writeStream = writeFileHandle.createWriteStream();
 
-  const encrypt = new Encrypt();
+  const decrypt = new Decrypt();
 
-  readStream.pipe(encrypt).pipe(writeStream);
+  readStream.pipe(decrypt).pipe(writeStream);
 })();
